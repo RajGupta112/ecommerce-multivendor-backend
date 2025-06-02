b122ac04eb2764e314c26cf58f8d6b22317e8da0
RajGupta112 <graj31105@gmail.com>
2025-06-02 20:36:37 +0530

    complete jwt authenr=tication

--- src/main/java/com/javafullstackEcommerce/ecommerce/config/AppConfig.java
+++ src/main/java/com/javafullstackEcommerce/ecommerce/config/AppConfig.java
@@ -29,7 +29,7 @@ public class AppConfig {
                         .anyRequest().permitAll()
                 ).addFilterBefore(new JwtTokenValidator(), BasicAuthenticationFilter.class)
                 .csrf(csrf-> csrf.disable())
-                .cors(cors->cors.configurationSource(corsConfgurationSource()))
+                .cors(cors->cors.configurationSource(corsConfgurationSource()));
 
                 return null;
     }
---
+++ src/main/java/com/javafullstackEcommerce/ecommerce/config/JWT_CONSTANT.java
@@ -0,0 +1,7 @@
+package com.javafullstackEcommerce.ecommerce.config;
+
+public class JWT_CONSTANT {
+    public  static final String SECRET_KEY="64d9bf75fa9157ff360905ccf308773698dd056cfb83ad958bbf6f6bcecfaeae";
+    public  static  final String  JWT_HEADER="Authorization";
+
+}
